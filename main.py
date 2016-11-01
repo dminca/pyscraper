2f9a5877d5278a4664601b9cd4840b9b14f83c9a
Daniel Andrei Minca <mandrei17@gmail.com>
2016-11-01 23:09:20 +0200

    print the results & limit python version
    * enable script only for python 2.7
    * print product prices
    * rename triggering function
    
    Resolves: #1
    Resolves: #2
    Signed-off-by: Daniel Andrei Minca <mandrei17@gmail.com>

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 # Simple Python web scraper to capture some elements in
-# HTML page (eg. price of a product)
+# HTML page (eg. price fluctuation of 3 products)
 from lxml import html
 import requests
 
-def func():
+def fetch():
     ssd_page = requests.get('http://www.emag.ro/solid-state-drive-ssd-samsung-850-evo-2-5-250gb-sata-iii-mz-75e250b-eu/pd/DCJ9BMBBM/')
     ram_page = requests.get('http://www.emag.ro/memorie-kingston-8gb-ddr3-1333mhz-non-ecc-cl9-1-5v-kvr1333d3n9-8g/pd/EBQRDBBBM/')
     rack_page = requests.get('http://www.emag.ro/rack-extern-njoy-speedbox-2-5-usb-3-0-negru-phas-2530s0s-ap01b/pd/D8FC6BBBM/')
@@ -17,7 +17,10 @@ def func():
     ram_price = tree.xpath('//*[@id="main-container"]/section[1]/div/div/div[1]/div[2]/div[2]/div/form/div/div[2]/div[1]/p/text()')
     rack_price = tree.xpath('//*[@id="main-container"]/section[1]/div/div/div[1]/div[2]/div[2]/div/form/div/div[2]/div[1]/p/text()')
 
+    print('250 GB Samsung SSD: ', ssd_price)
+    print('\n8 GB RAM DDR3 Kingston: ', ram_price)
+    print('\nnJoy Rack 2.5inch: ', rack_price)
+
 
 if __name__ == "__main__":
-    func()
-
+    fetch()
